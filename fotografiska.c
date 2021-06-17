5fd3863b6edb38a537c09689e3bf1ebfcad6cb95
Vlad-Ștefan Harbuz <vlad@vladh.net>
2021-06-17 11:50:37 +0200

    Update license

--- fotografiska.c
+++ fotografiska.c
@@ -1,3 +1,14 @@
+/*!
+  fotografiska
+
+  The author disclaims copyright to this source code.
+  In place of a legal notice, here is a blessing:
+
+  May you do good and not evil.
+  May you find forgiveness for yourself and forgive others.
+  May you share freely, never taking more than you give.
+*/
+
 // C lib headers
 #include <assert.h>
 #include <stdbool.h>
